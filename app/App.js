22d18da0e6ef3626b55b3bac7ff971fd71762850
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2019-05-18 17:22:41 +0200

    style(lint): no lint errors

--- app/App.js
+++ app/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    /* eslint no-console: 0 */
+    console.log('App Started');
+  }
+
   render() {
     return (
       <View style={styles.container}>
