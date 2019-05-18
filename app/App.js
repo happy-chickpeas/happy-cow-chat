4883d6881d1985756045c39d5659c25f4f47bb47
Kinga Magyar <kinga.magyar89@gmail.com>
2019-05-18 15:35:23 +0200

    Add Chat button to Inbox screen and redirects to Chat screen

--- app/App.js
+++ app/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Chat from './chat-view/Conversation';
+import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 
-export default class App extends React.Component {
+export class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Button 
+          color='#7C4EC4'
+          title='Chat' 
+          accessibilityLabel='Enter chat' 
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Chat' })
+              ],
+            }))
+          }}
+          />
         <Text>Mooo!</Text>
       </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Inbox: {
+    screen: App,
+  },
+  Chat: {
+    screen: Chat,
+  },
+}, {
+    initialRouteName: 'Inbox',
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default createAppContainer(AppNavigator);
---
+++ app/chat-view/Conversation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+
+class Chat extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>HappyCow Chat</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default Chat;
