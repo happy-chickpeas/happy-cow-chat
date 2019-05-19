4659d810435d1d5aaea8f43129de6e91ad59f484
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2019-05-19 16:58:48 +0200

    fix(inbox): missing screen

--- app/Components/InboxScreen.js
+++ app/Components/InboxScreen.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const InboxScreen = () => (
-  <View>
-    <Text>Inbox</Text>
-  </View>
-);
+export default class InboxScreen extends React.Component {
+  static navigationOptions = {
+    title: 'HappyCow Inbox',
+  };
 
-export default InboxScreen;
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          color='#7C4EC4'
+          title='Chat'
+          accessibilityLabel='Enter chat'
+          onPress={() => {
+            this.props.navigation.navigate('Chat')
+          }}
+          />
+        <Text>Mooo!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
